5d0207eab6d5dd5b1cdd24d7eb57838f27e875f3
Yuma Shintani <yuma.shintani@ntt.com>
2025-02-06 11:54:51 +0900

    refactoring

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,8 +1,5 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', {
